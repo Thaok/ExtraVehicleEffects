1079a5c5009f6855d4527859a5253800d12e73cb
Thaok <none@none>
2017-05-30 19:35:36 +0200

    Added missing effect definition sources.

---
+++ Effects/DieselIdling.cs
@@ -0,0 +1,50 @@
+﻿using ColossalFramework.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using ExtraVehicleEffects.GameExtensions;
+
+namespace ExtraVehicleEffects.Effects
+{
+    public class DieselIdling
+    {
+        private const string effectName = "Diesel Idling";
+
+        public static EffectInfo CreateEffectObject(Transform parent)
+        {
+            EngineSoundEffect defaultEngineSound = Util.FindEffect("Train Movement") as EngineSoundEffect;
+
+            if(defaultEngineSound != null)
+            {
+                GameObject obj = new GameObject(effectName);
+                obj.transform.parent = parent;
+
+                TurboDieselIdlingSoundEffect turboDieselEngineSound = TurboDieselIdlingSoundEffect.CopyEngineSoundEffect(defaultEngineSound, obj.AddComponent<TurboDieselIdlingSoundEffect>());
+
+                turboDieselEngineSound.name = effectName;
+
+                //init audio info
+                AudioInfo audioInfo = UnityEngine.Object.Instantiate(defaultEngineSound.m_audioInfo) as AudioInfo;
+                audioInfo.name = effectName;
+                var clip = Util.LoadAudioClipFromModDir("Sounds/diesel-engine-218-load-noTurbo.ogg");
+                if (clip != null)
+                {
+                    audioInfo.m_clip = clip;
+                    turboDieselEngineSound.m_audioInfo = audioInfo;
+                }
+                else
+                    return null;              
+
+                return turboDieselEngineSound;
+            }
+            else
+            {
+                Debug.Log("Could not find default train sound effect!");
+                return null;
+            }
+        }
+    }
+}
---
+++ Effects/TrainReverser.cs
@@ -0,0 +1,39 @@
+﻿using ColossalFramework.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using ExtraVehicleEffects.GameExtensions;
+
+namespace ExtraVehicleEffects.Effects
+{
+    public class TrainReverser
+    {
+        private const string effectName = "Train Reverser";
+        
+        
+        public static EffectInfo CreateEffectObject(Transform parent)
+        {
+            EngineSoundEffect defaultEngineSound = Util.FindEffect("Train Movement") as EngineSoundEffect;
+
+            if (defaultEngineSound != null)
+            {
+                GameObject obj = new GameObject(effectName);
+                obj.transform.parent = parent;
+
+                TurnTrainEffect newSoundEffect = TurnTrainEffect.CopyEngineSoundEffect(defaultEngineSound, obj.AddComponent<TurnTrainEffect>());
+                newSoundEffect.name = effectName;
+
+                return newSoundEffect;
+            }
+            else
+            {
+                Debug.Log("Could not find default train sound effect!");
+                return null;
+            }
+           
+        }
+    }
+}
